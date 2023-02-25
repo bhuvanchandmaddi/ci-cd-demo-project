81a75d46e98ff4289cb37b480fe66779395bb2d9
bhuvanchand <39622224+bhuvanchandmaddi@users.noreply.github.com>
2023-02-26 00:42:20 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,1 +1,5 @@
-docker run --rm  -v  /home/jenkins/jenkins/pipeline/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9-eclipse-temurin-17-alpine "$@"
+echo "***************************"
+echo "** Testing jar ***********"
+echo "***************************"
+
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9-eclipse-temurin-17-alpine "$@"
