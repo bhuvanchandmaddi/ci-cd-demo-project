074b9c9ca25d87ca359ed93ecef94fa1b32cac83
bhuvanchand <39622224+bhuvanchandmaddi@users.noreply.github.com>
2023-02-26 01:10:44 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -2,4 +2,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
+WORKSPACE=/home/jenkins/jenkins/pipeline/jenkins-data/workspace/maven-project
+
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9-eclipse-temurin-17-alpine "$@"
